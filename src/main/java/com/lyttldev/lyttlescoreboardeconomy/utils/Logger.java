35f4e65446ec3a9fdcbfcef03c1843b00f36d4dd
ThatsATim <tim.chiotis@gmail.com>
2024-07-12 19:16:29 +0200

    Use JavaPlugin logger over the Bukkit one and remove the redundant Logger.java file

--- src/main/java/com/lyttldev/lyttlescoreboardeconomy/utils/Logger.java
+++
@@ -1,9 +0,0 @@
-package com.lyttldev.lyttlescoreboardeconomy.utils;
-
-import org.bukkit.Bukkit;
-
-public class Logger {
-    public static void info(String message) {
-        Bukkit.getLogger().info(message);
-    }
-}
