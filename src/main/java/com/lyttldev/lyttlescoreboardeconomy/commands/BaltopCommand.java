5e63e6646d1492c97b6cbc8c00f65fc7e52b5ce6
tim <58576306+ThatsATim@users.noreply.github.com>
2025-06-01 18:50:12 +0200

    Remove an else

--- src/main/java/com/lyttldev/lyttlescoreboardeconomy/commands/BaltopCommand.java
+++ src/main/java/com/lyttldev/lyttlescoreboardeconomy/commands/BaltopCommand.java
@@ -34,54 +34,55 @@ public class BaltopCommand implements CommandExecutor, TabExecutor {
             return true;
         }
 
-        if (sender instanceof Player) {
-            Player player = (Player) sender;
-            int page = args.length > 0 ? Integer.parseInt(args[0]) : 1;
-            int pageSize = 10;
-            List<Map.Entry<String, Double>> topPlayers = getTopPlayers();
-            int pages = topPlayers.size() >= pageSize ? (int) Math.ceil(topPlayers.size() / pageSize) : 1;
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("This command can only be run by a player.");
+        }
 
-            Replacements.Builder replacements = Replacements.builder()
+        Player player = (Player) sender;
+        int page = args.length > 0 ? Integer.parseInt(args[0]) : 1;
+        int pageSize = 10;
+        List<Map.Entry<String, Double>> topPlayers = getTopPlayers();
+        int pages = topPlayers.size() >= pageSize ? (int) Math.ceil(topPlayers.size() / pageSize) : 1;
+
+        Replacements.Builder replacements = Replacements.builder()
                 .add("<PAGE>", String.valueOf(page))
                 .add("<PAGES>", String.valueOf(pages))
                 .add("<PAGE_SIZE>", String.valueOf(pageSize));
 
-            if (page < 1) {
-                plugin.message.sendMessage(sender, "baltop_page_zero", replacements.build(), player);
-                return true;
+        if (page < 1) {
+            plugin.message.sendMessage(sender, "baltop_page_zero", replacements.build(), player);
+            return true;
+        }
+
+        if (page > pages) {
+            plugin.message.sendMessage(sender, "baltop_page_maxed", replacements.build(), player);
+            return true;
+        }
+
+        replacements.add("<TOP_AMOUNT>", String.valueOf(10 * page));
+        Component title = plugin.message.getMessage("baltop_title", replacements.build(), player);
+        Component footer = plugin.message.getMessage("baltop_footer", replacements.build(), player);
+
+        StringBuilder message = new StringBuilder(mini.serialize(title));
+        for (int i = 0; i < pageSize; i++) {
+            if (i >= topPlayers.size()) {
+                break;
             }
-
-            if (page > pages) {
-                plugin.message.sendMessage(sender, "baltop_page_maxed", replacements.build(), player);
-                return true;
-            }
-
-            replacements.add("<TOP_AMOUNT>", String.valueOf(10 * page));
-            Component title = plugin.message.getMessage("baltop_title", replacements.build(), player);
-            Component footer = plugin.message.getMessage("baltop_footer", replacements.build(), player);
-
-            StringBuilder message = new StringBuilder(mini.serialize(title));
-            for (int i = 0; i < pageSize; i++) {
-                if (i >= topPlayers.size()) {
-                    break;
-                }
-                Map.Entry<String, Double> p = topPlayers.get(i + (page - 1) * pageSize);
-                int nr = i + 1 + (page - 1) * pageSize;
-                message.append("\n");
-                replacements
+            Map.Entry<String, Double> p = topPlayers.get(i + (page - 1) * pageSize);
+            int nr = i + 1 + (page - 1) * pageSize;
+            message.append("\n");
+            replacements
                     .add("<NR>", String.valueOf(nr))
                     .add("<PLAYER>", p.getKey())
                     .add("<AMOUNT>", String.valueOf(p.getValue()))
                     .add("<NUMBER>", String.valueOf(nr));
-                Component line = plugin.message.getMessage("baltop_line", replacements.build(), player);
-                message.append(mini.serialize(line));
-            }
-            message.append("\n").append(mini.serialize(footer));
-            replacements.add("<TOP_AMOUNT>", String.valueOf(10 * page));
-            plugin.message.sendMessageRaw(sender, mini.deserialize(message.toString()));
-        } else {
-            sender.sendMessage("This command can only be run by a player.");
+            Component line = plugin.message.getMessage("baltop_line", replacements.build(), player);
+            message.append(mini.serialize(line));
         }
+        message.append("\n").append(mini.serialize(footer));
+        replacements.add("<TOP_AMOUNT>", String.valueOf(10 * page));
+        plugin.message.sendMessageRaw(sender, mini.deserialize(message.toString()));
+
         return true;
     }
 
